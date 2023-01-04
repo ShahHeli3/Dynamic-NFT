7722dadacaac680600dad422421e3c79fdc5edee
ShahHeli3 <helis.inexture@gmail.com>
2023-01-04 10:45:08 +0530

    metadata can be updated

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+const main = async () => {
+    try {
+        const nftContractFactory = await hre.ethers.getContractFactory("DynamicNft");
+        const nftContract = await nftContractFactory.deploy();
+        await nftContract.deployed();
+
+        console.log("Contract deployed at:", nftContract.address);
+        process.exit(0);
+
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+main();
